09db68b92ea5a59b1c7dc1457dd4d4e84abb32bb
Percy Mamedy <percymamedy@gmail.com>
2016-07-12 17:23:21 +0400

    composer.lock added

--- tests/TestInsights.php
+++ tests/TestInsights.php
@@ -5,53 +5,26 @@ use FindBrok\PersonalityInsights\PersonalityInsights as PersonalityInsightsConcr
 use GuzzleHttp\Psr7\Response;
 use Orchestra\Testbench\TestCase;
 
-/**
- * Class TestInsights.
- */
 class TestInsights extends TestCase
 {
-    /**
-     * Content Items.
-     *
-     * @var array
-     */
-    public $contentItems = [];
-
-    /**
-     * Json Response mock.
-     *
-     * @var string
-     */
-    public $jsonResponse;
-
     /**
      * Watson Bridge.
      *
      * @var \FindBrok\WatsonBridge\Bridge
      */
     public $bridge;
-
     /**
-     * Setup Test.
+     * Content Items.
+     *
+     * @var array
      */
-    public function setUp()
-    {
-        parent::setUp();
-        //Mock Watson Bridge
-        $this->bridge = $this->getMockBuilder('FindBrok\WatsonBridge\Bridge')
-                             ->disableOriginalConstructor()
-                             ->setMethods(['post'])
-                             ->getMock();
-        //Mock Contents
-        $this->contentItems = json_decode(file_get_contents(__DIR__.'/Mocks/content-items.json'), true)['contentItems'];
-        //Mock Response Body
-        $this->jsonResponse = file_get_contents(__DIR__.'/Mocks/profile-response.json');
-
-        //Set return value of post method
-        $this->bridge->method('post')->withAnyParameters()->willReturn(new Response(200, [], $this->jsonResponse));
-        //Override Bridge in IOC
-        $this->app->instance('PersonalityInsightsBridge', $this->bridge);
-    }
+    public $contentItems = [];
+    /**
+     * Json Response mock.
+     *
+     * @var string
+     */
+    public $jsonResponse;
 
     /**
      * Get package providers.
@@ -63,6 +36,98 @@ class TestInsights extends TestCase
     protected function getPackageProviders($app)
     {
         return ['FindBrok\PersonalityInsights\InsightsServiceProvider'];
+    }
+
+    /**
+     * Setup Test.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        // Mock Watson Bridge
+        $this->bridge = $this->getMockBuilder('FindBrok\WatsonBridge\Bridge')
+                             ->disableOriginalConstructor()
+                             ->setMethods(['post'])
+                             ->getMock();
+        // Mock Contents
+        $this->contentItems = json_decode(file_get_contents(__DIR__ . '/Mocks/content-items.json'), true)['contentItems'];
+        // Mock Response Body
+        $this->jsonResponse = file_get_contents(__DIR__ . '/Mocks/profile-response.json');
+
+        // Set return value of post method
+        $this->bridge->method('post')->withAnyParameters()->willReturn(new Response(200, [], $this->jsonResponse));
+        // Override Bridge in IOC
+        $this->app->instance('PersonalityInsightsBridge', $this->bridge);
+    }
+
+    /**
+     * Test to see if we have Intellect insight in the Profile.
+     *
+     * @return bool
+     */
+    public function testCheckIfIntellectInsightsWithTrueAsAnswer()
+    {
+        // Get Insights
+        $insights = $this->app->make('PersonalityInsights')->addContentItems($this->contentItems);
+        // We have intellect so its true
+        $this->assertTrue($insights->hasInsight('Intellect', $insights->collectTree()));
+    }
+
+    /**
+     * Test to confirm that analysis is strong.
+     *
+     * @return void
+     */
+    public function testConfirmThatAnalysisIsVeryStrong()
+    {
+        // Analysis is strong
+        $this->assertTrue($this->app->make('PersonalityInsights')
+                                    ->addContentItems($this->contentItems)
+                                    ->isAnalysisStrong());
+    }
+
+    /**
+     * Test the Get Profile Function.
+     *
+     * @return void
+     */
+    public function testGetFullProfileWithExpectedProfile()
+    {
+        // Get Full Profile
+        $profile = $this->app->make('PersonalityInsights')->addContentItems($this->contentItems)->getFullProfile();
+        // See Full Profile
+        $this->assertJsonStringEqualsJsonString($this->jsonResponse, $profile->toJson());
+    }
+
+    /**
+     * Test that we get the expected percentage for Intellect.
+     *
+     * @return void
+     */
+    public function testGetIntellectInsightsWithExpectedPercentage()
+    {
+        // Get Intellect
+        $intellect = $this->app->make('PersonalityInsights')
+                               ->addContentItems($this->contentItems)
+                               ->getInsight('Intellect');
+        // We see the expected percentage
+        $this->assertEquals(41.7, $intellect->calculatePercentage());
+    }
+
+    /**
+     * Test that When we do not pass content item param we get an exception.
+     *
+     * @expectedException \FindBrok\PersonalityInsights\Exceptions\MissingParameterContentItemException
+     *
+     * @return void
+     */
+    public function testNoContentPassedToContentItemObjectMissingParameterExceptionThrown()
+    {
+        // Add content and exception is thrown here
+        $this->app->make('PersonalityInsights')->addContentItems([
+            'id'     => 'foo',
+            'source' => 'bar',
+        ]);
     }
 
     /**
@@ -91,71 +156,5 @@ class TestInsights extends TestCase
         );
         $profile = $insights->getFullProfile();
         $this->assertJsonStringEqualsJsonString($this->jsonResponse, $profile->toJson());
-    }
-
-    /**
-     * Test the Get Profile Function.
-     *
-     * @return void
-     */
-    public function testGetFullProfileWithExpectedProfile()
-    {
-        //Get Full Profile
-        $profile = $this->app->make('PersonalityInsights')->addContentItems($this->contentItems)->getFullProfile();
-        //See Full Profile
-        $this->assertJsonStringEqualsJsonString($this->jsonResponse, $profile->toJson());
-    }
-
-    /**
-     * Test to see if we have Intellect insight in the Profile.
-     *
-     * @return bool
-     */
-    public function testCheckIfIntellectInsightsWithTrueAsAnswer()
-    {
-        //Get Insights
-        $insights = $this->app->make('PersonalityInsights')->addContentItems($this->contentItems);
-        //We have intellect so its true
-        $this->assertTrue($insights->hasInsight('Intellect', $insights->collectTree()));
-    }
-
-    /**
-     * Test that we get the expected percentage for Intellect.
-     *
-     * @return void
-     */
-    public function testGetIntellectInsightsWithExpectedPercentage()
-    {
-        //Get Intellect
-        $intellect = $this->app->make('PersonalityInsights')->addContentItems($this->contentItems)->getInsight('Intellect');
-        //We see the expected percentage
-        $this->assertEquals(41.7, $intellect->calculatePercentage());
-    }
-
-    /**
-     * Test to confirm that analysis is strong.
-     *
-     * @return void
-     */
-    public function testConfirmThatAnalysisIsVeryStrong()
-    {
-        //Analysis is strong
-        $this->assertTrue($this->app->make('PersonalityInsights')->addContentItems($this->contentItems)->isAnalysisStrong());
-    }
-
-    /**
-     * Test that When we do not pass content item param we get an exception.
-     *
-     * @expectedException \FindBrok\PersonalityInsights\Exceptions\MissingParameterContentItemException
-     *
-     * @return void
-     */
-    public function testNoContentPassedToContentItemObjectMissingParameterExceptionThrown()
-    {
-        //Add content and exception is thrown here
-        $this->app->make('PersonalityInsights')->addContentItems([
-            'id'        => 'foo',
-            'source'    => 'bar',
-        ]);
     }
 }
