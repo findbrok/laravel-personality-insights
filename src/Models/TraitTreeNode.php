9a023bd14f9138d1059790ee4f1bad4271b86ddc
Percy Mamedy <percymamedy@gmail.com>
2017-01-19 22:25:48 +0400

    Added trait_id as test on TraiTreeNode

--- src/Models/TraitTreeNode.php
+++ src/Models/TraitTreeNode.php
@@ -4,5 +4,10 @@ namespace FindBrok\PersonalityInsights\Models;
 
 class TraitTreeNode
 {
-
+    /**
+     * The unique identifier of the characteristic to which the results pertain.
+     *
+     * @var string
+     */
+    public $trait_id;
 }
